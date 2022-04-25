8afa2db77283f795ec645be3897baa20286a14ee
unknown <nur-sin_sena@hotmai.com>
2022-04-25 17:07:12 +0300

    routing and doctorContext

--- dr-interpretation/src/api.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-export const fetchDoctorList = async () => {
-  const { data } = await axios.get(
-    "https://jsonplaceholder.typicode.com/users"
-  );
-  return data;
-};
