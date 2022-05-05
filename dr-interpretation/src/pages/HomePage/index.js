5fa402d54c445718674d6a27f6db45379c5ffb75
yavuuzsameet <yavuzsamet2000@gmail.com>
2022-05-05 16:12:39 +0300

    logout button added to the home page.

--- dr-interpretation/src/pages/HomePage/index.js
+++ dr-interpretation/src/pages/HomePage/index.js
@@ -4,6 +4,7 @@ import { useDoctor } from "../../context/DoctorContext";
 import { CustomSearch } from "../../component/CustomSearch";
 import { CustomHeader } from "../../component/CustomHeader";
 import { Cards } from "../../component/Cards";
+import { Link } from "react-router-dom";
 
 export const HomePage = () => {
   const { doctors, fetchDoctorIsLoading } = useDoctor();
@@ -22,6 +23,9 @@ export const HomePage = () => {
         <CustomSearch setTextInput={setTextInput} />
       </nav>
         <Cards data={doctorListByFilter} />
+        <button>
+        <Link to={"/"}>Logout</Link>
+      </button>
     </>
   );
 }
