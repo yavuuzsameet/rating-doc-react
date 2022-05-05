00ac68d014c9ea88ab10dda1b8a705aa015d6d85
Yavuz Samet Topçuoğlu <73655554+yavuuzsameet@users.noreply.github.com>
2022-05-06 01:43:05 +0300

    Update settings.py

--- react_backend/react_backend/settings.py
+++ react_backend/react_backend/settings.py
@@ -20,7 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-e%1x4-uc(@=z+gt#3%++#2d-h4f$p!@ot0&90$pee$jrla$zvi'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
